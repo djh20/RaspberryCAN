9cdd41a88423989f2136cd24b1bb11f6bede05bf
daniel <danmanmrdj@gmail.com>
2021-04-05 09:05:32 +1200

    update modules

--- modules/can.js
+++ modules/can.js
@@ -1,15 +1,29 @@
+const EventEmitter = require('events').EventEmitter;
 const socketcan = require('socketcan');
 
-class CanSocket {
+class CanSocket extends EventEmitter {
   constructor(interface_name, definition) {
+    super();
     this.interface_name = interface_name;
     this.definition = definition;
     this.channel = null;
-    this.values = {};
+    this.metrics = {};
   }
   
   connect() {
     this.channel = socketcan.createRawChannel(this.interface_name, true);
+    this.metrics = {};
+
+    if (!this.definition) return;
+
+    this.definition.messages.forEach((msg) => {
+      if (!msg.values) return;
+
+      msg.values.forEach((value) => {
+        let metric = new CanMetric(this, value);
+        this.metrics[value.name] = metric;
+      });
+    });
   }
 
   listen() {
@@ -19,8 +33,8 @@ class CanSocket {
       if (!this.definition || !this.definition.messages) return;
       
       for (let msg of this.definition.messages) {
-        if (frame.id == msg.id && msg.signals) {
-          this.process(frame, msg.signals);
+        if (frame.id == msg.id && msg.values) {
+          this.process(frame, msg.values);
         }
       }
     });
@@ -28,16 +42,49 @@ class CanSocket {
     this.channel.start();
   }
 
-  process(frame, signals) {
-    for (let signal of signals) {
-      if (!signal.process) continue;
-  
-      let value = signal.process(frame.data);
-      if (value != this.values[signal.name]) {
-        this.values[signal.name] = value;
-        console.log(`> ${signal.name}: ${value}`)
-      }
+  process(frame, values) {
+    for (let value of values) {
+      let metric = this.metrics[value.name];
+      if (metric) metric.update(frame);
     }
+  }
+}
+
+class CanMetric {
+  constructor(socket, params) {
+    this.socket = socket;
+    this.id = params.id;
+    this.name = params.name;
+    this.process = params.process;
+    this.convert = params.convert;
+    this.log = params.log === false ? false : true;
+    this.value = 0;
+  }
+
+  update(frame) {
+    let val = this.process(frame.data);
+    if (val != this.value) { 
+      this.value = val;
+      this.socket.emit('update', this);
+    }
+  }
+
+  asByteArray() {
+    //let id_array = new Uint8Array([this.id]);
+    //let value_array = this.convert ? this.convert(this.value) : new Uint8Array([this.value]);
+    //let array = new Uint8Array(value_array.buffer, value_array.byteOffset, value_array.byteLength);
+
+    let data = this.convert ? this.convert(this.value) : new Uint8Array([this.value]);
+    
+    if (data.byteLength >= 2) {
+      data = new Uint8Array(data.buffer, data.byteOffset, data.byteLength);
+    }
+
+    let merged = new Uint8Array(data.length + 1);
+    merged.set(new Uint8Array([this.id]));
+    merged.set(data, 1);
+    
+    return merged
   }
 }
 
