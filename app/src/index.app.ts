5f2c99bbed1bfd4e050dc8e55baf759461654fb9
daniel <danmanmrdj@gmail.com>
2021-05-08 22:45:33 +1200

    2021.05-5

--- app/src/index.app.ts
+++ app/src/index.app.ts
@@ -1,7 +1,7 @@
 import App from './app/App';
 import Logger from './util/Logger';
 
-const VERSION = "2021.05-4d";
+const VERSION = "2021.05-5";
 
 console.log(`RaspberryCAN (${VERSION})`)
 Logger.info('App', `Starting...`);
